bba8eb9c230b63dad1770bf7136f71c38b83154d
EUNSOLLEE <aiffel.es@gmail.com>
2025-04-23 18:40:47 +0900

    250423,18:40-은솔 작업 저장

--- lib/main.dart
+++ lib/main.dart
@@ -27,7 +27,7 @@ Future<void> main() async {
 
   // 환경변수 로드
   final isEmulator = await _isRunningOnEmulator();
-  await dotenv.load(fileName: '.env.template');
+  await dotenv.load(fileName: '.env');
 
   runApp(
     const ProviderScope(
