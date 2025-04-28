5cb9f0ff5f29066110a4c1354cc8eec32503c544
EUNSOLLEE <aiffel.es@gmail.com>
2025-04-28 20:03:00 +0900

    250428,20:00-은솔 작업 저장(오늘의 뉴스)
    Former-commit-id: d80e8a73d203e965aa2a248fbdd4a61188dc09c0

---
+++ lib/viewmodels/youtube_viewmodel.dart
@@ -0,0 +1,7 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import '../services/youtube_service.dart';
+import '../models/youtube_video.dart';
+
+final youtubeNewsProvider = FutureProvider<List<YoutubeVideo>>((ref) async {
+  return await YoutubeService.fetchLaborNewsVideos();
+});
