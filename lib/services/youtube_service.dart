94edb85c686f406307eaaf40a7d025e971fe0246
EUNSOLLEE <aiffel.es@gmail.com>
2025-04-30 11:01:19 +0900

    250430,10:10-은솔 작업 저장(뉴스 출처 지정)

--- lib/services/youtube_service.dart
+++ lib/services/youtube_service.dart
@@ -2,30 +2,48 @@ import 'package:http/http.dart' as http;
 import 'dart:convert';
 import '../models/youtube_video.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'dart:math';
 
 class YoutubeService {
   static final _apiKey = dotenv.env['YOUTUBE_API_KEY'] ?? '';
   static const _baseUrl = 'https://www.googleapis.com/youtube/v3/search';
 
-  /// 노동 관련 유튜브 뉴스 가져오기
-  static Future<List<YoutubeVideo>> fetchLaborNewsVideos({String order = 'date'}) async {
+  static final List<String> _keywords = ['직장 뉴스', '노동 뉴스', '기업 뉴스', '고용 뉴스'];
+
+  static Future<List<YoutubeVideo>> fetchLaborNewsVideos() async {
+    final keyword = (_keywords..shuffle()).first;
+    final oneWeekAgo = DateTime.now().subtract(const Duration(days: 30));
+    final publishedAfter = oneWeekAgo.toUtc().toIso8601String();
+
     final response = await http.get(
       Uri.parse('$_baseUrl?part=snippet'
-          '&q=직장 뉴스'
+          '&q=$keyword'
           '&type=video'
-          '&order=$order'
+          '&order=date'
+          '&publishedAfter=$publishedAfter'
           '&maxResults=10'
           '&key=$_apiKey'),
     );
 
-    print('✅ 뉴스 응답 상태 코드: ${response.statusCode}');
-    print('✅ 뉴스 응답 바디: ${response.body}');
-
     if (response.statusCode == 200) {
       final data = json.decode(response.body);
-      return (data['items'] as List)
+      final videos = (data['items'] as List)
           .map((item) => YoutubeVideo.fromJson(item))
+          .where((video) => [
+        'KBS',
+        'MBC',
+        'SBS',
+        'JTBC',
+        'YTN',
+        '연합뉴스',
+        'MBN',
+        'TV조선',
+        '채널A',
+        '한국경제TV',
+      ].any((name) => video.channelTitle?.contains(name) ?? false))
           .toList();
+
+      return videos;
     } else {
       throw Exception('유튜브 뉴스 로딩 실패');
     }
@@ -37,6 +55,7 @@ class YoutubeService {
       Uri.parse(
         'https://www.googleapis.com/youtube/v3/playlistItems'
             '?part=snippet'
+            '&order=date'
             '&maxResults=10'
             '&playlistId=$playlistId'
             '&key=$_apiKey',
